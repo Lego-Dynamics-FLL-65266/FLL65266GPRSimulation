448dbb074c5555c8115201a4dc188692d4cc85fb
alkadienePhoton <atharvna12@gmail.com>
2026-05-08 18:10:28 -0500

    Removed unnecessary code.
    A lot was obsolete...

--- wifi.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require("ws");
-
-// Connect directly to the ESP32's AP IP
-const ws = new WebSocket("ws://192.168.4.1:81");
-
-ws.on("open", () => console.log("Screaming data received!"));
-ws.on("message", (data) => {
-  console.log(`GY-521 Says: ${data}`);
-});
